4cb7c3b043bc187ead3b84daef63a13b8b249fc3
Arne Lehmann <arne.lehmann@gmx.de>
2017-06-05 20:34:09 +0200

    started with the easy controller

--- Controllers/KeyFilesController.fs
+++ Controllers/KeyFilesController.fs
@@ -1,9 +1,12 @@
 namespace EpisodeWebMvc.Controllers
 
 open System
+open System.IO
 open Microsoft.AspNetCore.Mvc
 open Newtonsoft.Json
 open Newtonsoft.Json.Linq
+
+open TvApi
 
 [<Route("api/keyfiles")>]
 type KeyFilesController() =
@@ -11,5 +14,13 @@ type KeyFilesController() =
 
     [<HttpGet>]
     member this.Get() =
-        JObject.Parse("{'someother':'value'}")
+        let files = Directory.GetFiles(@"z:\downloads\done", "*.otrkey")
+                    |> Seq.map FileInfo
+                    |> Seq.sortBy(fun i -> i.CreationTime)
+                    |> Seq.map(fun i -> i.Name)
+
+        let response = JObject()
+        response.Add("files", JToken.FromObject(files))
+
+        response
 
