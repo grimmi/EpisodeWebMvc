32f74a82245bb855a58cfd9d0d4dcd94d86c7775
Arne Lehmann <arne.lehmann@gmx.de>
2017-06-07 20:24:25 +0200

    removed the old way of getting the jobservice

--- Controllers/DecodeJobController.fs
+++ Controllers/DecodeJobController.fs
@@ -8,17 +8,10 @@ open Microsoft.AspNetCore.Mvc
 [<Route("api/decodejob")>]
 type DecodeJobController(jobService : JobService) =
     inherit Controller()
-    static let mutable jobService:JobService option = None
-
-    let getService =
-        match jobService with
-        |None -> jobService <- Some(JobService())
-                 jobService.Value
-        |_ -> jobService.Value
+    let service = jobService
 
     [<HttpGet>]
     member this.Get() =         
-        let service = getService
         let job = service.CurrentJob
         match job with
         |None -> let errResponse = JObject()
@@ -28,8 +21,6 @@ type DecodeJobController(jobService : JobService) =
 
     [<HttpPost>]
     member this.Post(files: string) =
-        let service = getService
-        
         let ops = files.Split([|','|])
         service.Run ops
 
