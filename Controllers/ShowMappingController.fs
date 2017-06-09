09c2c5478aa7d1c83014c5639c614b13b51e1f24
Arne Lehmann <arne.lehmann@gmx.de>
2017-06-09 22:15:54 +0200

    able to add show mappings (at least in memory for the time being)

--- Controllers/ShowMappingController.fs
+++ Controllers/ShowMappingController.fs
@@ -1,5 +1,7 @@
 namespace EpisodeWebMvc.Controllers
 
+open Newtonsoft.Json
+open Newtonsoft.Json.Linq
 open Microsoft.AspNetCore.Mvc
 open System
 open System.IO
@@ -7,12 +9,19 @@ open System.IO
 [<Route("api/showmapping")>]
 type ShowMappingController() =
     inherit Controller()
-    let showMappings = 
+
+    let loadMappings = 
         File.ReadAllLines("./shows.map")
         |> Seq.map(fun (l:string) -> match l.Split("***", StringSplitOptions.RemoveEmptyEntries) with
-                                     |[|parsed; mapped; id|] -> (parsed,mapped,id)
+                                     |[|parsed; mapped; id|] -> (parsed.Trim(),mapped.Trim(),id.Trim())
                                      |_ -> ("","",""))
+        |> Seq.filter(fun triple -> triple <> ("","",""))
+    let mutable showMappings = loadMappings
 
 
     [<HttpPost>]
-    member this.Post(parsed: string, mapped: string) = ()
+    member this.Post(parsed: string, mapped: string) = 
+        showMappings <- showMappings |> Seq.append [(parsed, mapped, "")]
+        let response = JObject()
+        response.Add("message", JToken.FromObject(sprintf "added mapping: %s --> %s" parsed mapped))
+        response
