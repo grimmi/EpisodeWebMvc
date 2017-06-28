6b35345a84551a7e568de4b4c615ef0e98d31931
Arne Lehmann <arne.lehmann@gmx.de>
2017-06-28 20:08:28 +0200

    controller to do it all

---
+++ Controllers/ProcessController.fs
@@ -0,0 +1,38 @@
+namespace EpisodeWebMvc.Controllers
+
+open EpisodeWebMvc
+open System
+open System.IO
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Options
+open Newtonsoft.Json
+open Newtonsoft.Json.Linq
+open ShowFinder
+open EpisodeFinder
+open JsonTransformations
+
+[<Route("api/process")>]
+type ProcessController(service: JobService) =
+    inherit Controller()
+
+    [<HttpGet>]
+    member this.Get() =
+        match service.CurrentJob with
+        |None -> let response = JObject()
+                 response.Add("message", JToken.FromObject("no job running"))
+                 response
+        |Some job -> job.ToJson()
+
+    [<HttpPost>]
+    member this.Post(infos : JObject seq) =
+        let f = infos
+                |> Seq.map(fun info -> { episodename = info.Value<string>("episodename");
+                                        episodenumber = info.Value<int>("episodenumber");
+                                        show = info.Value<string>("show");
+                                        season = info.Value<int>("season");
+                                        file = info.Value<string>("file"); })
+        // service.RunProcess f
+
+        match service.CurrentJob with
+        |Some job -> job.ToJson()
+        |_ -> JObject.Parse("{\"error\":\"unable to create job\"}")
