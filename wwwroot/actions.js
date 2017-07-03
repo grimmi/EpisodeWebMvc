ff5d2e9e1b9170d23a8665f282ce59f4a89f1000
Arne Lehmann <arne.lehmann@gmx.de>
2017-07-03 20:53:43 +0200

    removed logging

--- wwwroot/actions.js
+++ wwwroot/actions.js
@@ -6,7 +6,6 @@ function loadFiles(){
     req.setRequestHeader("Content-Type", "application/json");
     req.onload = function(e){
         fileinfos = [];
-        console.log(req.responseText);
         var infos = JSON.parse(req.responseText)["infos"];
         for(var i = 0; i < infos.length; i++){
             var info = infos[i];
@@ -15,10 +14,6 @@ function loadFiles(){
             var file = info["file"];
 
             fileinfos.push({"show": show["name"], "episodename": episode["name"], "episodenumber": episode["number"], "season": episode["season"], "file": file });
-        }
-
-        for(var j = 0; j < fileinfos.length; j++){
-            console.log(fileinfos[j]);
         }
     }
     req.send(null);
